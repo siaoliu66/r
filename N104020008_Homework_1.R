0fc8491f91c55073d8e30e95009101849ff0d5a3
siaoliu66 <63837974+siaoliu66@users.noreply.github.com>
2022-04-29 18:04:34 +0800

    Add files via upload

--- N104020008_Homework_1.R
+++ N104020008_Homework_1.R
@@ -18,9 +18,25 @@ survey$Height = cut(x= survey$Height,breaks=c(150,160,170,180,190,Inf),labels=c(
 ### costable_survey 這個變數可以自行改名，其他用取代方式一次取代
 costable_survey = table(survey$Exer,survey$Height)
 costable_survey
+
 # 1.2-2 將身高分類 找出身高跟練習頻率的關係
+recoding_Height <- function(x) {
+  if (x >= 150 && x < 160) {
+    Ht_group <- "150"
+  } else if(x >= 160 && x < 170){
+    Ht_group <- "160"
+  }else if(x >= 170 && x < 180){
+    Ht_group <- "170"
+  }else if(x >= 180 && x < 190){
+    Ht_group <- "180"
+  }else{
+    Ht_group <- "190"
+  }
+  return(Ht_group)
+}
+survey$Height_New <- sapply(FUN = recoding_Height, X = survey$Height)
 library(gmodels) #加載gmodels包
-CrossTable(survey$Exer,survey$Height,expected = T,format = "SPSS",fisher = T,prop.c = F,prop.t = F,prop.chisq = F)
+CrossTable(survey$Exer,survey$Height_New,expected = T,format = "SPSS",fisher = T,prop.c = F,prop.t = F,prop.chisq = F)
 
 
 # 1.3 假設檢定
@@ -32,7 +48,7 @@ fisher.test(costable_survey,simulate.p.value = T)
 
 # Q2
 my_StudentsPerformance <- read_csv("my_StudentsPerformance.csv")
-# 2.1 寫一個function 算平均、最大值、最小值...
+# 2.1 寫一個my_summary 的function 計算平均、最大值、最小值和中值
 my_summary = function(x) {
   return(data.frame(mean = mean(x),sd = sd(x),max = max(x),min = min(x),med = median(x)))
 }
@@ -40,21 +56,34 @@ my_summary(my_StudentsPerformance$math_score) # math_score
 my_summary(my_StudentsPerformance$reading_score) # reading_score
 my_summary(my_StudentsPerformance$writing_score) # writing_score
 
-# 2.2 計算平均分數並加入新欄
-###  avg_score 這個變數可以自行改名，其他用取代方式一次取代
+# 2.2 計算數學、閱讀和寫作的平均分數並加入新欄avg_score
 my_StudentsPerformance$avg_score = rowMeans(my_StudentsPerformance[,c("math_score","reading_score","writing_score")])
 
-# 2.3 性別跟成績是否相關 anova
+# 2.3-1 性別跟成績是否相關 anova
 ggplot(aes(gender,avg_score),data = my_StudentsPerformance)+geom_boxplot()
 ggplot(aes(avg_score,color = gender),data = my_StudentsPerformance)+geom_density(alpha = 0.1)+xlim(0,100)
 aov.my_StudentsPerformance = aov(avg_score~gender,data=my_StudentsPerformance)
 summary(aov.my_StudentsPerformance)
+# 2.3-2 性別跟成績是否相關 常態檢定
+ggplot(aes(gender,avg_score),data = my_StudentsPerformance)+geom_boxplot()
+ggplot(aes(avg_score,color = gender),data = my_StudentsPerformance)+geom_density(alpha = 0.1)+xlim(0,100)
+# 檢查是否趨近於常態 shapiro.test(x)
+shapiro.test(avg_score)  # p<0.05 不是常態
+t.test(avg_score~gender,data=my_StudentsPerformance) # T檢定 p<0.05 不是常態 或 分布不一樣
+willcox.tes(avg_score~gender,data=my_StudentsPerformance) # 雙樣本中位數檢定 p<0.05 不是常態
 
 # 2.4 雙親教育程度跟成績是否相關 anova
+### tb_parental 這個變數可以自行改名，其他用取代方式一次取代
 tb_parental = table(my_StudentsPerformance$parental_level_of_education);
 prop.table(tb_parental)
+
+lm_score = lm(avg_score~parental_level_of_education,data=my_StudentsPerformance) #一般線性迴歸
+
 aov.parental_score = aov(avg_score~parental_level_of_education,data=my_StudentsPerformance)
 summary(aov.parental_score)
+### anova不準確所以多做一個檢定
+willcox.tes(avg_score~parental_level_of_education,data=my_StudentsPerformance) #  p<0.05 
+
 
 # Q3
 diamonds = fread("diamonds.csv",data.table = F)
